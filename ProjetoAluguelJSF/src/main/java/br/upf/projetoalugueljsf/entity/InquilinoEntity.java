f8ef0ae715585eeb6eef79242d9b4e0fe5665c77
unknown <pedrodelemosdamini@gmail.com>
2024-06-19 23:35:24 -0300

    ajustando inquilino

--- ProjetoAluguelJSF/src/main/java/br/upf/projetoalugueljsf/entity/InquilinoEntity.java
+++ ProjetoAluguelJSF/src/main/java/br/upf/projetoalugueljsf/entity/InquilinoEntity.java
@@ -6,23 +6,29 @@ package br.upf.projetoalugueljsf.entity;
 
 import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.Date;
+import java.util.Objects;
 
 /**
  *
  * @author Pedro Damini
  */
+@Entity
+@Table(name = "inquilino")
 public class InquilinoEntity {
     
+    private static final long serialVersionUID = 1L;
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -56,6 +62,50 @@ public class InquilinoEntity {
     @JoinColumn(name = "id_apartamento", referencedColumnName = "id")
     @ManyToOne(optional = false)
     private ApartamentoEntity idApartamento;
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + this.id;
+        hash = 37 * hash + Objects.hashCode(this.nome);
+        hash = 37 * hash + Objects.hashCode(this.cpf);
+        hash = 37 * hash + Objects.hashCode(this.dataNascimento);
+        hash = 37 * hash + Objects.hashCode(this.mesinquilino);
+        hash = 37 * hash + Objects.hashCode(this.idApartamento);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final InquilinoEntity other = (InquilinoEntity) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.cpf, other.cpf)) {
+            return false;
+        }
+        if (!Objects.equals(this.dataNascimento, other.dataNascimento)) {
+            return false;
+        }
+        if (!Objects.equals(this.mesinquilino, other.mesinquilino)) {
+            return false;
+        }
+        return Objects.equals(this.idApartamento, other.idApartamento);
+    }
+    
+    
 
     public int getId() {
         return id;
